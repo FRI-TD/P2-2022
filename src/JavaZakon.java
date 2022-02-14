fba5565e74bcc50ddb122bc8c8e0cde5a2f386af
Tomaz Dobravec <tomaz.dobravec@fri.uni-lj.si>
2022-02-14 10:57:10 +0100

    p1

---
+++ src/JavaZakon.java
@@ -0,0 +1,11 @@
+public class JavaZakon {
+	public static void main(String[] args) {
+		// daljsi nacin ...
+		// int i;
+		// for(i = 1; i <= 15; i = i + 1) {
+
+		for(int i = 1; i <= 15; i++) {
+  		  System.out.println(i + ". Java je ZAKON!");
+  		}
+	}
+}
