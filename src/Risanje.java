1f95b0e62c5f133735d759ee0093cf37fa9873a6
Tomaz Dobravec <dobravec@gmail.com>
2022-04-04 12:02:28 +0200

    .

--- src/Risanje.java
+++ src/Risanje.java
@@ -1,6 +1,10 @@
 import java.awt.*;
+import java.util.Date;
 
-// Za risanje potrebujemo knjiznico stdlib.jar
+// Opomba: razred Risanje se nahaja v privzetem paketu (torej NI v paketu, nahaja se kar v korenskem 
+// direktoriju projekta), saj sicer knjižnica stdlib.jar ne dela. Če želite razred zapisati v kak drug 
+// paket (recimo v predavanja06), morate namesto stdlib.jar uporabiti knjižnico algs4.jar 
+// (obe knjižnici sta dostopni na eUčilnici). 
 
 public class Risanje {
 
@@ -44,7 +48,136 @@ public class Risanje {
         }
     }
 
+    static void nRoza(int n){
+        StdDraw.setScale(-100,100);
+
+        double kot = 2 * Math.PI  / n ;
+        double trX = 0;
+        double trY = 0;
+        double trKot = 0;
+        double d = 20;
+
+        for(int j=0; j<n; j++) {
+            for (int i = 0; i < n; i++) {
+                double nX = trX + (i < n - 1 ? d : 3 * d) * Math.cos(trKot);
+                double nY = trY + (i < n - 1 ? d : 3 * d) * Math.sin(trKot);
+
+                StdDraw.line(trX, trY, nX, nY);
+                StdDraw.pause(100);
+                if (i < n-1)
+                  trKot = trKot + kot;
+                trX = nX;
+                trY = nY;
+            }
+        }
+
+    }
+
+    static void spirala() {
+        StdDraw.setScale(-100,100);
+
+       double fi = 0;
+       double r  = 1;
+
+       double deltaFi = 5 * 2*Math.PI / 360;
+       double deltaR = 1.01;
+
+       double trX = 0;
+       double trY = 0;
+
+       for (int i=0; i<1000; i++) {
+           double nX = Math.cos(fi)*r;
+           double nY = Math.sin(fi)*r;
+           StdDraw.line(trX, trY, nX, nY);
+
+           fi = fi + deltaFi;
+           r  = r  * deltaR;
+
+           trX = nX; trY = nY;
+       }
+    }
+
+    static void fri() {
+        StdDraw.setScale(0,100);
+        StdDraw.enableDoubleBuffering();
+
+        for (int i = 0; i < 1000 ; i++) {
+            StdDraw.clear();
+            StdDraw.text(i/10.0, 50, "FRI");
+
+            StdDraw.show();
+
+        }
+    }
+
+    static void kazalec(double kot, double dolzina, double debelina) {
+        StdDraw.setPenRadius(debelina);
+        kot = 90 - kot;
+
+        StdDraw.line(0,0, dolzina * Math.cos(2*Math.PI*kot/360), dolzina * Math.sin(2*Math.PI*kot/360));
+    }
+
+    static void radar() {
+        StdDraw.setScale(-100,100);
+        StdDraw.enableDoubleBuffering();
+        double kot = 0;
+        while (true) {
+            StdDraw.clear(Color.black);
+
+            StdDraw.setPenColor(Color.green);
+            kazalec(kot, 80, 0.01);
+            kot = kot + 1;
+
+            StdDraw.show();
+            StdDraw.pause(1000);
+        }
+    }
+
+    static void ura() {
+        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("hh:mm:ss");
+
+        StdDraw.setScale(-100,100);
+
+        StdDraw.enableDoubleBuffering();
+        while (true) {
+            StdDraw.clear();
+            String ura = sdf.format(new Date());
+
+            String[] uraDeli = ura.split(":");
+            int hh = Integer.parseInt(uraDeli[0]) % 12;  // 0, 1, 2, ... 11
+            int mm = Integer.parseInt(uraDeli[1]);
+            int ss = Integer.parseInt(uraDeli[2]);
+
+            for (int i=0; i<12; i++) {
+                double kot = (90 - i*30)*(2*Math.PI/360);
+                double x = 93*Math.cos(kot);
+                double y = 93*Math.sin(kot);
+                StdDraw.text(x,y, Integer.toString(i == 0 ? 12 : i));
+
+                StdDraw.setPenRadius(0.03);
+                StdDraw.line(80*Math.cos(kot), 80*Math.sin(kot), 85*Math.cos(kot), 85*Math.sin(kot));
+
+            }
+
+            for (int i=0; i<60; i++) {
+                double kot = (90 - i*6)*(2*Math.PI/360);
+                StdDraw.setPenRadius(0.005);
+                StdDraw.line(82*Math.cos(kot), 82*Math.sin(kot), 85*Math.cos(kot), 85*Math.sin(kot));
+            }
+
+            kazalec(hh * 30, 70, 0.03);
+            kazalec(mm * 6, 80, 0.02);
+            kazalec(ss * 6, 85, 0.01);
+
+
+            StdDraw.text(-80, 90, ura);
+
+            StdDraw.show();
+            StdDraw.pause(1000);
+        }
+    }
+
     public static void main(String[] args) {
-       r3();
+        ura();
     }
 }
